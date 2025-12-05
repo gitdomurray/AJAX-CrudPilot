ab7d7429e3db65ebde535266232e59d9867e0438
Arthur "AskeladdeN" Murray <arthur.dartora.tds2023@gmail.com>
2025-12-05 14:11:17 -0300

    README

--- PilotCrud/dao/EquipeDAO.php
+++ PilotCrud/dao/EquipeDAO.php
@@ -6,12 +6,12 @@ class EquipeDAO {
         $this->pdo = $pdo;
     }
 
-    // Lista todas as equipes
+    // listagem
     public function listar() {
         return $this->pdo->query("SELECT * FROM equipe")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // Lista equipes filtradas por categoria
+    // listagem por categorias 
     public function listarPorCategoria($categoria_id) {
         $stmt = $this->pdo->prepare("SELECT * FROM equipe WHERE categoria_id = :categoria_id");
         $stmt->execute(['categoria_id' => $categoria_id]);
